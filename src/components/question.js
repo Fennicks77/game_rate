8d85471c5ac169864459e3fa37e198a8a10302d5
Nikita Tuev <fenn77@MacBook-Air-fenn77.local>
2017-04-04 11:36:47 +0300

    abrakadabra

---
+++ src/components/question.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+export default class Question extends Component {
+  static propTypes = {}
+
+  static defaultProps = {}
+
+  state = {}
+
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h1 className='question-number'>Вопрос №{this.props.number}</h1>
+          </div>
+          <div className="col-md-12">
+            <p className='question-text'>{this.props.text}</p>
+          </div>
+          <div className="col-md-6 text-center">
+            <button className="question-answer-yes btn btn-success" onClick={() => this.props.onAnswer(true)}>
+              Да
+            </button>
+          </div>
+          <div className="col-md-6 text-center">
+            <button className="question-answer-no btn btn-warning" onClick={() => this.props.onAnswer(false)}>
+              Нет
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
